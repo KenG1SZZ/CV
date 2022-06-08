1cbb99d53703f7ceb64f1da70e0de0d2341fcad2
KenG1SZZ <sultanzhalgas@gmail.com>
2022-06-08 17:45:30 +0600

    закончить методы и проход по массивам XML данных чтобы отправить все это в таблицы БД в MYSQL

--- venv/test.py
+++ venv/test.py
@@ -32,7 +32,7 @@ IIKO_URL = 'https://bahandi-co.iiko.it/resto/api'
 IIKO_LOGIN = 'Keng1'
 IIKO_PASSWORD = '37a7d5806f9d502b67bc96109eaa91918ac1d53b'
 
-iiko_log = IIkoServer(IIKO_LOGIN, IIKO_PASSWORD, IIKO_URL)
+iiko_log = IikoServer(IIKO_LOGIN, IIKO_PASSWORD, IIKO_URL)
 IIKO_VERSION = iiko_log.get_version()
 IIKO_TOKEN = iiko_log.auth()
 
@@ -42,89 +42,106 @@ nextdate = date.today()
 str_date = todaydate.strftime("%Y-%m-%d")
 strn_date = nextdate.strftime("%Y-%m-%d")
 
-def get_oversales ():
-
-    overall_sales = iiko_cleint.casshift_sum_report(str_date)
-    parse_overs = ET.fromstring(overall_sales)
-    data_overs = parse_overs.findall('date')
-
-    for i in data_overs:
-        date = i.find('v/cls=java.util.Date типо такого')
-        department = i.find('v/cls=java.lang.String')
-        cash_sum = i.find('v/cls=java.math.BigDecimal')
-    table = """INSERT INTO overall_sales(date,department,cash_sum) VALUES(%s,%s,%s)"""
-
-    for i in table:
-        c = conn.cursor()
-        c.execute(table, (date, department, cash_sum))
-        conn.commit()
+# def get_oversales ():
+#
+#     overall_sales = iiko_cleint.casshift_sum_report(str_date)
+#     parse_overs = ET.fromstring(overall_sales)
+#     data_overs = parse_overs.findall('date')
+#
+#     # from xml.etree import ElementTree as ET
+#     # tree = ET.parse("doc.xml")
+#     # root = tree.getroot()
+#     # for et in root.iter("i"):
+#     #     if len(data := et.findall("v")) == 3:
+#     #         department, amount, service = (x.text for x in data)
+#     for i in data_overs:
+#         date = i.find('v/cls=java.util.Date типо такого')
+#         department = i.find('v/cls=java.lang.String')
+#         cash_sum = i.find('v/cls=java.math.BigDecimal')
+#
+#     table = """INSERT INTO overall_sales(date,department,cash_sum) VALUES(%s,%s,%s)"""
+#
+#     for i in table:
+#         c = conn.cursor()
+#         c.execute(table, (date, department, cash_sum))
+#         conn.commit()
+#
 def get_aggrsales ():
 
     aggr_sales = iiko_cleint.casshift_by_aggregators(str_date, strn_date)
     parse_aggr = ET.fromstring(aggr_sales)
     data_aggr = parse_aggr.findall('date')
 
-    for i in data_aggr:
-        date = i.find('v/cls=java.util.Date типо такого')
-        department = i.find('k department v/cls=java.lang.String')
-        paytype = i.find('k paytype v/cls=java.lang.String')
-        cash_sum = i.find('v/cls=java.lang.BigDecimal')
-    table = """INSERT INTO overall_sales(date,department,cash_sum) VALUES(%s,%s,%s)"""
+    from xml.etree import ElementTree as ET
+    tree = ET.parse("doc.xml")
+    root = tree.getroot()
+    for et in root.iter("i"):
+        if len(data := et.findall("v")) == 3:
+            department, cash_sum, paytype = (x.text for x in data)
+
+    table = """INSERT INTO aggr_sales(department,cash_sum,paytype) VALUES(%s,%s,%s)"""
 
     for i in table:
         c = conn.cursor()
-        c.execute(table, (date, department, cash_sum))
+        c.execute(table, (department, cash_sum, paytype))
         conn.commit()
 
-def get_pricecost ():
+# def get_pricecost ():
+#
+#     price_cost = iiko_cleint.cost_price(str_date, strn_date)
+#     parse_pcost = ET.fromstring(price_cost)
+#     data_pc = parse_pcost.findall('date')
+#
+#     for i in data_pc:
+#         date = i.find('v/cls=java.util.Date типо такого')
+#         store_id = i.find()
+#         over_sales = i.find()
+#         cost_price = i.find('v/cls=java.math.BigDecimal')
+#     table = """INSERT INTO price_cost(date,store_id,over_sales,cost_price) VALUES(%s,%s,%s,%s,%s)"""
+#
+#     for i in table:
+#         c = conn.cursor()
+#         c.execute(table, date, store_id, over_sales, cost_price)
+#         conn.commit()
 
-    price_cost = iiko_cleint.cost_price(str_date, strn_date)
-    parse_pcost = ET.fromstring(price_cost)
-    data_overs = parse_pcost.findall('date')
-
-    for i in data_overs:
-        date = i.find('v/cls=java.util.Date типо такого')
-        department = i.find('v/cls=java.lang.String')
-
-        cash_sum = i.find('v/cls=java.math.BigDecimal')
-    table = """INSERT INTO overall_sales(date,department,cash_sum) VALUES(%s,%s,%s)"""
-
-    for i in table:
-        c = conn.cursor()
-        c.execute(table, (date, department, cash_sum))
-        conn.commit()
-
-def get_storeitems():
-    overall_sales = iiko_cleint.casshift_sum_report(str_date, strn_date)
-    parse_overs = ET.fromstring(overall_sales)
-    data_overs = parse_overs.findall('date')
-
-    for i in data_overs:
-        date = i.find('v/cls=java.util.Date типо такого')
-        department = i.find('v/cls=java.lang.String')
-        cash_sum = i.find('v/cls=java.math.BigDecimal')
-    table = """INSERT INTO overall_sales(date,department,cash_sum) VALUES(%s,%s,%s)"""
-
-    for i in table:
-        c = conn.cursor()
-        c.execute(table, (date, department, cash_sum))
-        conn.commit()
-
-def get_emplshift ():
-    overall_sales = iiko_cleint.casshift_sum_report(str_date, strn_date)
-    parse_overs = ET.fromstring(overall_sales)
-    data_overs = parse_overs.findall('date')
-
-    for i in data_overs:
-        date = i.find('v/cls=java.util.Date типо такого')
-        department = i.find('v/cls=java.lang.String')
-        cash_sum = i.find('v/cls=java.math.BigDecimal')
-    table = """INSERT INTO overall_sales(date,department,cash_sum) VALUES(%s,%s,%s)"""
-
-    for i in table:
-        c = conn.cursor()
-        c.execute(table, (date, department, cash_sum))
-        conn.commit()
+# def get_storeitems():
+#     store_items = iiko_cleint.casshift_sum_report(str_date, strn_date)
+#     parse_si = ET.fromstring(store_items)
+#     data_si = parse_si.findall('date')
+#
+#     for i in data_si:
+#         document_id = i.find()
+#         date = i.find('v/cls=java.util.Date типо такого')
+#         number = i.find()
+#         store = i.find('v/cls=java.lang.String')
+#         amount = i.find()
+#         diff = i.find()
+#         surplus = i.find()
+#         shortage = i.find()
+#
+#     table = """INSERT INTO overall_sales(document_id,date,number,store,amount,diff,surplus,shortage) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"""
+#
+#     for i in table:
+#         c = conn.cursor()
+#         c.execute(table, (document_id, date, number, store, amount, diff, surplus, shortage))
+#         conn.commit()
+#
+# def get_emplshift ():
+#     empl_shift = iiko_cleint.casshift_sum_report(str_date, strn_date)
+#     parse_es = ET.fromstring(empl_shift)
+#     data_es = parse_es.findall('date')
+#
+#     for i in data_es:
+#         id_emp = i.find('v/cls=java.util.Date типо такого')
+#         department_id = i.find('v/cls=java.lang.String')
+#         date = i.find('v/cls=java.math.BigDecimal')
+#         department_name = i.find('v/cls=java.math.BigDecimal')
+#     table = """INSERT INTO empl_shift(date,department_id,department_name,id_emp) VALUES(%s,%s,%s,%s)"""
+#
+#     for i in table:
+#         c = conn.cursor()
+#         c.execute(table, (date, department_id, department_name, id_emp))
+#         conn.commit()
     #creating the cursor object
 
 
